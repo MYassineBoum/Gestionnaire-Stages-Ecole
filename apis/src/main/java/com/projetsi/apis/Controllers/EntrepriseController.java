0debf5e99d94a887720d4391d0c8075d1f94fc38
joyluff <saberfadli750@gmail.com>
2023-12-15 01:57:01 +0100

    back

--- apis/src/main/java/com/projetsi/apis/Controllers/EntrepriseController.java
+++ apis/src/main/java/com/projetsi/apis/Controllers/EntrepriseController.java
@@ -1,4 +1,44 @@
 package com.projetsi.apis.Controllers;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.projetsi.apis.Entities.Entreprise;
+import com.projetsi.apis.Repositories.EntrepriseRepository;
+
 public class EntrepriseController {
+    @Autowired
+    private EntrepriseRepository entrepriseRepository;
+
+    @PostMapping("/api/entreprise/ajouter")
+    public ResponseEntity<Entreprise> ajouterentreprise(@RequestBody Entreprise entreprise) {
+        return ResponseEntity.ok(entrepriseRepository.save(entreprise));
+    }
+
+    @PostMapping("/api/entreprise/supprimer")
+    public ResponseEntity<String> supprimerentreprise(@PathVariable("no_entreprise") Long no_entreprise) {
+        Optional<Entreprise> entreprise = entrepriseRepository.findById(no_entreprise);
+        entrepriseRepository.delete(entreprise.get());
+        return new ResponseEntity<String>("entreprise supprimé!", HttpStatus.OK);
+    }
+
+    @PostMapping("/api/entreprise/modifier")
+    public ResponseEntity<Entreprise> modifierentreprise(@RequestBody Entreprise entreprise) {
+        Optional<Entreprise> entrepriseTrouvé = entrepriseRepository.findById(entreprise.getNo_entreprise());
+        entrepriseRepository.delete(entrepriseTrouvé.get());
+        return ResponseEntity.ok(entrepriseRepository.save(entreprise));
+    }
+
+    @GetMapping("/api/entreprise/liste")
+    public ResponseEntity<List<Entreprise>> listerentreprises() {
+        return ResponseEntity.ok(entrepriseRepository.findAll());
+    }
 }
--- apis/src/main/java/com/projetsi/apis/Controllers/StageController.java
+++ apis/src/main/java/com/projetsi/apis/Controllers/StageController.java
@@ -1,4 +1,44 @@
 package com.projetsi.apis.Controllers;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.projetsi.apis.Entities.Stage;
+import com.projetsi.apis.Repositories.StageRepository;
+
 public class StageController {
+    @Autowired
+    private StageRepository stageRepository;
+
+    @PostMapping("/api/stage/ajouter")
+    public ResponseEntity<Stage> ajouterStage(@RequestBody Stage stage) {
+        return ResponseEntity.ok(stageRepository.save(stage));
+    }
+
+    @PostMapping("/api/stage/supprimer")
+    public ResponseEntity<String> supprimerStage(@PathVariable("no_stage") Long no_stage) {
+        Optional<Stage> stage = stageRepository.findById(no_stage);
+        stageRepository.delete(stage.get());
+        return new ResponseEntity<String>("Stage supprimé!", HttpStatus.OK);
+    }
+
+    @PostMapping("/api/stage/modifier")
+    public ResponseEntity<Stage> modifierStage(@RequestBody Stage stage) {
+        Optional<Stage> stageTrouvé = stageRepository.findById(stage.getNo_stage());
+        stageRepository.delete(stageTrouvé.get());
+        return ResponseEntity.ok(stageRepository.save(stage));
+    }
+
+    @GetMapping("/api/stage/liste")
+    public ResponseEntity<List<Stage>> listerStages() {
+        return ResponseEntity.ok(stageRepository.findAll());
+    }
 }
